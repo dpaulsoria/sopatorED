d58675bb6c0014d0c0f9353c5baa1b2940fc45a0
Paul Soria <dpaulsoria@gmail.com>
2021-11-21 13:14:19 -0500

    Clase Sopa

--- sopaletras/src/main/java/espol/sopaletras/sopa.java
+++ sopaletras/src/main/java/espol/sopaletras/sopa.java
@@ -5,12 +5,27 @@
  */
 package espol.sopaletras;
 
+import java.util.HashMap;
+
 /**
  *
  * @author danny
  */
 public class sopa {
-    CircularLinkedList filas;
+    final int filas;
+    final int columnas;
+    HashMap<Integer, CircularLinkedList> sopa = new HashMap<>();;
+    
+    public sopa(int filas, int columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+    }
+    
+    public void setFilas(int fila, CircularLinkedList list) {
+        if (fila > -1 && list != null) {
+            sopa.put(fila, list);
+        }
+    }
     
     
 }
