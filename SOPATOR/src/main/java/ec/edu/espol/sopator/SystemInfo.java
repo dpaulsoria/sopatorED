ccdf5b5c416e614673077ce248a390ac903c8c53
jnicolascr <jncarras@espol.edu.ec>
2021-11-23 23:37:23 -0500

    Creando nuevo proyecto

---
+++ SOPATOR/src/main/java/ec/edu/espol/sopator/SystemInfo.java
@@ -0,0 +1,13 @@
+package ec.edu.espol.sopator;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ SOPATOR/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+/* doesn't work with source level 1.8:
+module ec.edu.espol.sopator {
+    requires javafx.controls;
+    exports ec.edu.espol.sopator;
+}
+*/
