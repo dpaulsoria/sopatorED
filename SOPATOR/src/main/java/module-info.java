51802fec5cc12be4ff3b525bd625592f94234f5b
jnicolascr <jncarras@espol.edu.ec>
2021-11-24 10:18:31 -0500

    Añadiendo todo

---
+++ SOPATOR/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module ec.edu.espol.sopator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens ec.edu.espol.sopator to javafx.fxml;
+    exports ec.edu.espol.sopator;
+}
+*/
